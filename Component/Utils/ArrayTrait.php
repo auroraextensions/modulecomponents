82c4a7cd90d879b49fa1bf8c5c2679858a9ab4f0
Nickolas Burr <nickolasburr@gmail.com>
2020-03-16 14:29:13 -0400

    Add ArrayTrait

---
+++ Component/Utils/ArrayTrait.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * ArrayTrait.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT license, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/modulecomponents/LICENSE.txt
+ *
+ * @package       AuroraExtensions_ModuleComponents
+ * @copyright     Copyright (C) 2020 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\ModuleComponents\Component\Utils;
+
+/**
+ * @api
+ */
+trait ArrayTrait
+{
+    /**
+     * @param array $data
+     * @return array
+     */
+    public function flattenArray(array $data = []): array
+    {
+        /** @var array $result */
+        $result = [];
+
+        /** @var mixed $value */
+        foreach ($data as $value) {
+            if (is_array($value)) {
+                $result = array_merge(
+                    $result,
+                    $this->flattenArray($value)
+                );
+            } else {
+                $result[] = $value;
+            }
+        }
+
+        return $result;
+    }
+}
