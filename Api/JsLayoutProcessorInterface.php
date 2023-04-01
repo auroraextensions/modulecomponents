0e3305583d3d3291e3f590ac197b32021e433443
Nickolas Burr <nickolasburr@gmail.com>
2023-03-31 22:09:04 -0400

    Add JsLayoutProcessorInterface interface

---
+++ Api/JsLayoutProcessorInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * JsLayoutProcessorInterface.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT license, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/modulecomponents/LICENSE.txt
+ *
+ * @package     AuroraExtensions\ModuleComponents\Api
+ * @copyright   Copyright (C) 2023 Aurora Extensions <support@auroraextensions.com>
+ * @license     MIT
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\ModuleComponents\Api;
+
+interface JsLayoutProcessorInterface
+{
+    /**
+     * @param array $jsLayout
+     * @return array
+     */
+    public function process(array $jsLayout): array;
+}
